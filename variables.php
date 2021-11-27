a81c149831fd33a52f90ef30dea745d413031f48
suprabhat25 <suprabhat2018@gmail.com>
2021-11-27 20:52:01 +0530

    [added] strings

--- variables.php
+++ variables.php
@@ -1,6 +1,6 @@
 <?php
 
-$name="Suprabhat";
+$name="Suprabhat  ";  // white spaces are counted in strlen.
 $income = 200000000;
 echo var_dump($name); // string(9) "Suprabhat"  //var_dump tells data type, length of string and value. 
 echo "<br>";
@@ -21,4 +21,19 @@ $friends = array("abcd","efgh","ijkl","mnop");
 echo var_dump($friends);   // for printing the details of array.
 echo "<br>";
 echo $friends[0];
+echo "<br>";
+
+// String
+$name = "Kumar is my name";
+echo "The salary is " . $name;
+echo "<br>";
+echo "The length is " . strlen($name) . " and the word count is " . str_word_count($name); // str_word_count($name) gives the words count.
+echo "<br>";
+echo strrev($name);  // to reverse the string.
+echo "<br>";
+echo strpos($name, "is"); // output is 6. // strpos counts whitespaces too.
+echo "<br>";
+echo str_replace("Kumar","Suprabhat",$name); // replace "Kumar" with "Suprabhat" in $name.
+
+
 ?>
