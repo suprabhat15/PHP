c356288f8e761f937b09d165057dd433d64eaaf3
suprabhat25 <suprabhat2018@gmail.com>
2021-11-27 19:51:34 +0530

    [added] var_dump, variables' rules, array

--- variables.php
+++ variables.php
@@ -2,5 +2,23 @@
 
 $name="Suprabhat";
 $income = 200000000;
-echo "My name is $name and salary is $income";
+echo var_dump($name); // string(9) "Suprabhat"  //var_dump tells data type, length of string and value. 
+echo "<br>";
+$check = true;
+echo var_dump($check); // bool(true)  // for bool values, var_dump tells data type and value.
+echo "<br>My name is $name and salary is $income";
+
+// more about variables
+echo "<br> variables start with a $ symbol";
+echo "<br> variables' name don't start with a number";
+echo "<br> variables' name start with a character, underscore";
+echo "<br> variables contain alphanumeric";
+echo "<br> variables are case sensitive.";
+echo "<br>";
+
+// Arrays
+$friends = array("abcd","efgh","ijkl","mnop");
+echo var_dump($friends);   // for printing the details of array.
+echo "<br>";
+echo $friends[0];
 ?>
