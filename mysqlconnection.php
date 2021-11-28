687addd1dee55f780f487dfb6ac4a7703fd8fdf1
suprabhat25 <suprabhat2018@gmail.com>
2021-11-29 01:32:05 +0530

    creation of mysql database using php

--- mysqlconnection.php
+++ mysqlconnection.php
@@ -13,6 +13,26 @@ $conn = mysqli_connect($server, $username, $password);
 if(!$conn){
     die("Server Connection Error :". mysqli_connect_error());
 }
-echo "Connection is successful";
+echo "Connection is successful <br>";
+
+
+// Create a database
+$sql = "CREATE DATABASE kumarDB1";
+$result = mysqli_query($conn, $sql);
+
+if($result){
+    echo "Database created successfully <br>";
+}
+else{
+    echo "Database Creation Failure : " . mysqli_error($conn);
+    echo "<br>";
+}
+
+echo "The result is ";
+echo var_dump($result);  // when the database is created, it returns true;
+echo "<br>";
+
+
+
 
 ?>
