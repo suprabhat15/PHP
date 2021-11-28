acf22ac1437d6ccdd602e349c4172f2bf2cc935b
suprabhat25 <suprabhat2018@gmail.com>
2021-11-29 01:12:56 +0530

    [added] mysql connection & error handling while connecting

---
+++ mysqlconnection.php
@@ -0,0 +1,18 @@
+<?php
+// Ways to connect to a MYSQL Database
+//  1. mysqli extension
+//  2. PDO
+
+// Connecting to a Database
+$server = "localhost";
+$username = "root";
+$password = " ";
+
+// Create a connection 
+$conn = mysqli_connect($server, $username, $password);
+if(!$conn){
+    die("Server Connection Error :". mysqli_connect_error());
+}
+echo "Connection is successful";
+
+?>
